56bdd117f5ddc72df7cdeae33214df5acc767f4a
Masya Smerdov <kuvalda-max@mail.ru>
2021-07-06 18:00:23 +0300

    Остановился на парсере файла

--- profile/personal_area/downland_files/up_file.php
+++ profile/personal_area/downland_files/up_file.php
@@ -9,8 +9,8 @@
   ?>
 </head>
 <body>
+  <!-- Блок загрузки файла -->
   <div class="upload_file" style="text-align: center;">
-<!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
 <form enctype="multipart/form-data" action="upload.php" method="POST">
     <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
     <input type="hidden" name="MAX_FILE_SIZE" value="210000" />
@@ -18,10 +18,14 @@
     Загружай файл пидрила: <input name="userfile" type="file" /> <br> <br>
     Город: <input name="sity" type="text" />
     <input type="submit" value="Отправить файл" />
+    <?
+echo "\n Некоторая отладочная информация:";
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
+  ?>
 </form>
   </div>
-  <?
 
-  ?>
 </body>
 </html>
