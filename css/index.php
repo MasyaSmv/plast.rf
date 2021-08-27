e74380c69dc5e243f614713b71e0c04ac7dc7f48
MasyaSm <kuvalda-max@mauil.ru>
2021-08-27 17:58:09 +0300

    Тесты дизайна

--- css/index.php
+++ css/index.php
@@ -1,62 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
-    integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous">
-    </script>
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
-</head>
-
-<body>
-    <nav class="navbar fixed-top navbar-light bg-light Mi4Ko9">
-        <a class="navbar-brand" href="#">
-            <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-            Пластикаты РФ
-        </a>
-    </nav>
-    <!-- The sidebar -->
-    <!-- <div class="sidebar HhU8Ld">
-        <div class="hh8Yt">
-            <div class="Pl8UnT">
-                <a class="sideLink" href="#">Новости ПВХ</a>
-            </div>
-            <div class="Pl8UnT">
-                <a class="sideLink" href="#">Каталог компаний</a>
-            </div>
-            <div class="Pl8UnT">
-                <a class="sideLink" href="#">Карта производства</a>
-            </div>
-            <div class="Pl8UnT">
-                <a class="sideLink" href="#">Справочник пвх</a>
-            </div>
-            <div class="Pl8UnT">
-                <a class="sideLink" href="#">Стандарты/ГОСТы</a>
-            </div>
-            <div class="Pl8UnT">
-                <a class="sideLink" href="#">Литература</a>
-            </div>
-            <div class="Pl8UnT">
-                <a class="sideLink" href="#">О проекте</a>
-            </div>
-        </div>
-    </div> -->
-        <!-- МОбильное боковое меню -->
-        <div class="T4LgNb">
+<?php
+    //Подключение шапки
+    require_once("header.php");
+?>
+       <!-- МОбильное боковое меню -->
+       <div class="T4LgNb">
         <div class="VjFXz"></div>
         <div class="RJy67e UUlDsf">
             <div class="wrDwse" style="top: 56px;">
@@ -268,6 +215,35 @@
                 </div>
             </div>
         </div>
-</body>
-
-</html>
+        <!-- тело страницы -->
+<div class="container">
+    <div class="row">
+        <div class="col">
+        <div class="col-6">
+            <div class="wrtefg" style="border: 1px solid; padding-bottom: 2em;">
+                <div class="head">
+                    <h1>wsdgsdf</h1>
+                </div>
+                <div class="body">
+                    <h3>wsdgsdf</h3>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+        <div class="col-6 Ergg">
+            <div class="wrtefg">
+            <div class="head">
+            <h1 class="secondH" itemprop="name">Производство полимерных автокомпонентов в РФ локализовано почти на 50%</h1>
+                </div>
+                <div class="body">
+                    <h3>wsdgsdf</h3>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    //Подключение шапки
-    require_once("header.php");
-?>
-<div>
-    <h1>Huetaaa</h1>
-</div>
